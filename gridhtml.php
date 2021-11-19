97cf725400d815f62626c04945461b6bb4426fc6
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2021-11-19 16:41:41 +0100

    Update 2.0.2

--- gridhtml.php
+++ gridhtml.php
@@ -74,7 +74,7 @@ class GridHtml extends ModuleGridEngine
         } else {
             $this->name = 'gridhtml';
             $this->tab = 'administration';
-            $this->version = '2.0.1';
+            $this->version = '2.0.2';
             $this->author = 'PrestaShop';
             $this->need_instance = 0;
 
